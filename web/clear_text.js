3ef1bf5fc6a24dd927e299eab6d2528fd964166c
m0rtus59 <ad28m95@gmail.com>
2026-07-26 10:32:22 +0500

    Fixed Quickstart node in nodes 2.0 mode

--- web/clear_text.js
+++ web/clear_text.js
@@ -8,11 +8,12 @@ app.registerExtension({
             const onExecuted = nodeType.prototype.onExecuted;
             nodeType.prototype.onExecuted = function (message) {
                 onExecuted?.apply(this, arguments);
-                const textWidget = this.widgets.find((w) => w.name === "text");
+                const textWidget = this.widgets?.find((w) => w.name === "text");
                 if (textWidget) {
                     textWidget.value = "";
-                    if (textWidget.callback) textWidget.callback("");
-                    this.setDirtyCanvas(true);
+                    if (textWidget._state) textWidget._state.value = "";
+                    if (typeof textWidget.callback === "function") textWidget.callback("");
+                    if (typeof this.setDirtyCanvas === "function") this.setDirtyCanvas(true);
                 }
             };
         }
@@ -23,47 +24,47 @@ app.registerExtension({
             nodeType.prototype.onNodeCreated = function () {
                 onNodeCreated?.apply(this, arguments);
                 
-                // Hide the trigger_state widget
-                const triggerWidget = this.widgets.find((w) => w.name === "trigger_state");
+                // Hide trigger_state widget
+                const triggerWidget = this.widgets?.find((w) => w.name === "trigger_state");
                 if (triggerWidget) {
                     triggerWidget.type = "converted-widget";
                     triggerWidget.computeSize = () => [0, -4];
-                    if (triggerWidget.inputEl) {
-                        triggerWidget.inputEl.style.display = "none";
-                    }
+                    if (triggerWidget.inputEl) triggerWidget.inputEl.style.display = "none";
                 }
                 
-                // Add the Submit button
-                this.addWidget("button", "Submit Prompt", null, () => {
-                    const triggerWidget = this.widgets.find((w) => w.name === "trigger_state");
-                    if (triggerWidget) {
-                        triggerWidget.value = true;
+                // Add Submit button
+                this.addWidget("button", "Submit Prompt", null, (val, canvas, targetNode) => {
+                    const node = targetNode || this;
+                    const tw = node.widgets?.find((w) => w.name === "trigger_state");
+                    if (tw) {
+                        tw.value = true;
+                        if (tw._state) tw._state.value = true;
                     }
                     app.queuePrompt(0);
                 });
             };
 
-            // Reset state and clear input based on execution results
             const onExecuted = nodeType.prototype.onExecuted;
             nodeType.prototype.onExecuted = function (message) {
                 onExecuted?.apply(this, arguments);
-                
                 const triggered = message?.trigger_state?.[0];
                 const autoClear = message?.auto_clear?.[0] !== false;
                 
                 if (triggered) {
-                    const triggerWidget = this.widgets.find((w) => w.name === "trigger_state");
-                    if (triggerWidget) {
-                        triggerWidget.value = false;
+                    const tw = this.widgets?.find((w) => w.name === "trigger_state");
+                    if (tw) {
+                        tw.value = false;
+                        if (tw._state) tw._state.value = false;
                     }
                     if (autoClear) {
-                        const textWidget = this.widgets.find((w) => w.name === "text");
+                        const textWidget = this.widgets?.find((w) => w.name === "text");
                         if (textWidget) {
                             textWidget.value = "";
-                            if (textWidget.callback) textWidget.callback("");
+                            if (textWidget._state) textWidget._state.value = "";
+                            if (typeof textWidget.callback === "function") textWidget.callback("");
                         }
                     }
-                    this.setDirtyCanvas(true);
+                    if (typeof this.setDirtyCanvas === "function") this.setDirtyCanvas(true);
                 }
             };
         }
@@ -73,41 +74,88 @@ app.registerExtension({
             const onNodeCreated = nodeType.prototype.onNodeCreated;
             nodeType.prototype.onNodeCreated = function () {
                 onNodeCreated?.apply(this, arguments);
+                const node = this;
                 
-                // Hide the "value" widget completely to keep layout perfectly clean
-                const valueWidget = this.widgets.find((w) => w.name === "value");
+                // Hide "value" widget and neutralize canvas mouse drag/drawing on it
+                const valueWidget = node.widgets?.find((w) => w.name === "value");
                 if (valueWidget) {
                     valueWidget.type = "converted-widget";
                     valueWidget.computeSize = () => [0, -4];
+                    valueWidget.draw = () => {};
+                    valueWidget.mouse = () => false;
                     if (valueWidget.inputEl) {
                         valueWidget.inputEl.style.display = "none";
+                        valueWidget.inputEl.style.pointerEvents = "none";
                     }
                 }
                 
-                // Add the custom button displaying only the current numeric value
                 const initialValue = valueWidget ? valueWidget.value : 0;
-                const restartBtn = this.addWidget("button", String(initialValue), null, () => {
+                const initialStr = String(initialValue);
+
+                // Add custom button displaying the current numeric value
+                const restartBtn = node.addWidget("button", initialStr, initialStr, function (val, canvas, targetNode) {
+                    const actualNode = targetNode || node;
+
                     // Safe 53-bit random integer generator
                     const newValue = Math.floor(Math.random() * 9007199254740991);
-                    if (valueWidget) {
-                        valueWidget.value = newValue;
+                    const newStr = String(newValue);
+
+                    // Update hidden value widget
+                    const valWidget = actualNode.widgets?.find((w) => w.name === "value");
+                    if (valWidget) {
+                        valWidget.value = newValue;
+                        if (valWidget._state) valWidget._state.value = newValue;
                     }
-                    restartBtn.name = String(newValue);
-                    this.setDirtyCanvas(true);
+
+                    // Update button label across Litegraph and Vue
+                    const btnWidget = actualNode.widgets?.find((w) => w.type === "button") || restartBtn;
+                    if (btnWidget) {
+                        btnWidget.name = newStr;
+                        btnWidget.label = newStr;
+                        btnWidget.value = newStr;
+                        if (btnWidget._state) {
+                            btnWidget._state.name = newStr;
+                            btnWidget._state.label = newStr;
+                            btnWidget._state.displayName = newStr;
+                            btnWidget._state.value = newStr;
+                        }
+                    }
+
+                    if (typeof actualNode.setDirtyCanvas === "function") actualNode.setDirtyCanvas(true, true);
+                    if (app.graph?.change) app.graph.change();
                     
-                    // Fire the queue prompt immediately to reset/restart the session
                     app.queuePrompt(0);
                 });
-                this.restartBtn = restartBtn;
+
+                // Dynamic getters for Vue and Litegraph mode compatibility
+                const getValStr = () => String(valueWidget ? valueWidget.value : 0);
+                try {
+                    Object.defineProperties(restartBtn, {
+                        name: { get: getValStr, set: () => {}, configurable: true },
+                        label: { get: getValStr, set: () => {}, configurable: true },
+                        displayName: { get: getValStr, set: () => {}, configurable: true }
+                    });
+                } catch (e) {}
+
+                node.restartBtn = restartBtn;
             };
 
-            // Restore the correct numeric value onto the button name when loading/configuring
             const onConfigure = nodeType.prototype.onConfigure;
             nodeType.prototype.onConfigure = function (info) {
                 onConfigure?.apply(this, arguments);
-                const valueWidget = this.widgets.find((w) => w.name === "value");
-                if (valueWidget && this.restartBtn) {
-                    this.restartBtn.name = String(valueWidget.value);
+                const valueWidget = this.widgets?.find((w) => w.name === "value");
+                const btnWidget = this.widgets?.find((w) => w.type === "button") || this.restartBtn;
+                if (valueWidget && btnWidget) {
+                    const strVal = String(valueWidget.value);
+                    btnWidget.name = strVal;
+                    btnWidget.label = strVal;
+                    btnWidget.value = strVal;
+                    if (btnWidget._state) {
+                        btnWidget._state.name = strVal;
+                        btnWidget._state.label = strVal;
+                        btnWidget._state.displayName = strVal;
+                        btnWidget._state.value = strVal;
+                    }
                 }
             };
         }
